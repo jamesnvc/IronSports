f83700bfa9e4363722bd4bd3c0d9e929e318fba8
James N. V. Cash <james.nvc@gmail.com>
2012-03-17 11:42:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UofTIronSports::Application.config.secret_token = 'b66fbbf340f1205304110dbdf7eb0907b1d2be5fa386fb6579e0d8741bbc9b4feb61416e1d963048600d413fd8a466bbfc28351d072782f45239dcfcf480a8a7'
