ff37189638d936ec7bba44d7d24a48664ab7f82f
James N. V. Cash <james.nvc@gmail.com>
2012-08-22 13:26:56 -0400

    Add user methods for wilks & total

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,48 @@ class User < ActiveRecord::Base
   attr_protected :is_admin
 
   validates_format_of :gender, with: /\A(M|F)\z/, message: "M or F for gender"
-  validates_numericality_of :bodyweight
   validates_uniqueness_of :email
 
   def admin?
     return self.is_admin
+  end
+
+  def wilks
+    coeffs = {
+        'M' => {
+          a: -216.0475144,
+          b: 16.2606339,
+          c: -0.002388645,
+          d: -0.00113732,
+          e: 7.01863e-6,
+          f: -1.29e-8
+      },
+        'F' => {
+          a: 594.31747775582,
+          b: -27.23842536447,
+          c: 0.82112226871,
+          d: -0.00930733913,
+          e: 0.00004731582,
+          f: -0.00000009054
+      }
+    }
+    c = coeffs[self.gender]
+    return 500.0 / (
+      c[:a] +
+      c[:b] * self.bodyweight    +
+      c[:c] * self.bodyweight**2 +
+      c[:d] * self.bodyweight**3 +
+      c[:e] * self.bodyweight**4 +
+      c[:f] * self.bodyweight**5
+    )
+  end
+
+  def total
+    return [ squat_max || 0, bench_max || 0, deadlift_max || 0 ].sum
+  end
+
+  def wilks_total
+    return wilks * total
   end
 
   def name
