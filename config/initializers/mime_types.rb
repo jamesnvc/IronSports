05a72e655f398fafb2a4544f2966c638db734d80
James N. V. Cash <james.nvc@gmail.com>
2013-01-10 12:05:15 -0500

    Addressing terrifying rails vuln

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,3 +4,6 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 Mime::Type.register 'font/opentype', :font
+
+# Address horrific RoR bug
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
